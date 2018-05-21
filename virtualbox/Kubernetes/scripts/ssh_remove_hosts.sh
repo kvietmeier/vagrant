b734526fbb3bb2409d73e7e8bc49d37051887d27
Karl Vietmeier <9323750+kvietmeier@users.noreply.github.com>
2018-05-20 20:21:39 -0700

    Fixed devops Vagrantfile

--- virtualbox/Kubernetes/scripts/ssh_remove_hosts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Remove hosts from known_hosts
-
-for i in $(cat /etc/hosts | egrep 'centos|ubuntu' | awk '{print $1}'); do ssh-keygen -R $i; done
-for i in $(cat /etc/hosts | egrep 'centos|ubuntu' | awk '{print $2}'); do ssh-keygen -R $i; done
