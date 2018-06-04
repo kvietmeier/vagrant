06191473e70a87ae094e6195cf956642c1f02ded
Karl Vietmeier <9323750+kvietmeier@users.noreply.github.com>
2018-06-04 12:04:52 -0700

    working on provisioning

--- virtualbox/scratch/Vagrantfile1.rb
+++ virtualbox/scratch/Vagrantfile1.rb
@@ -102,5 +102,29 @@ ubuntu.vm.provider "virtualbox" do |virtualbox|
     ]
   end
 
-
   
+private_key_path = File.join(Dir.home, ".ssh", "id_rsa")
+public_key_path = File.join(Dir.home, ".ssh", "id_rsa.pub")
+insecure_key_path = File.join(Dir.home, ".vagrant.d", "insecure_private_key")
+
+private_key = IO.read(private_key_path)
+public_key = IO.read(public_key_path)
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "ubuntu/trusty64"
+  config.ssh.insert_key = false
+  config.ssh.private_key_path = [
+    private_key_path,
+    insecure_key_path # to provision the first time
+  ]
+
+  config.vm.provision :shell, :inline => <<-SCRIPT
+    set -e
+
+    echo '#{private_key}' > /home/vagrant/.ssh/id_rsa
+    chmod 600 /home/vagrant/.ssh/id_rsa
+
+    echo '#{public_key}' > /home/vagrant/.ssh/authorized_keys
+    chmod 600 /home/vagrant/.ssh/authorized_keys
+  SCRIPT
+en
