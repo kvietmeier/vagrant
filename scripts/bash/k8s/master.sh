9dacdf59933df56ce23e9976eb432b8d7a2d50d8
Karl Vietmeier <9323750+kvietmeier@users.noreply.github.com>
2018-07-05 17:08:34 -0700

    Configuring Provisioning

---
+++ scripts/bash/k8s/master.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This does it old schoola and adds users to the sudoers list
+# Probably better to use - "newusers /vagrant/userlist.txt"
+
+#
+
+echo "Configuring Master Node"
+echo "Master" >> /home/vagrant/file.txt
---
+++ scripts/bash/k8s/worker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This does it old schoola and adds users to the sudoers list
+# Probably better to use - "newusers /vagrant/userlist.txt"
+
+#
+
+echo "Configuring Worker Node"
+echo "Worker" >> /home/vagrant/file.txt
