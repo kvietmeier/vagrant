767730f163191347389eb8ee71c8ebcfaaeabc69
Karl Vietmeier <9323750+kvietmeier@users.noreply.github.com>
2018-07-13 15:22:59 -0700

    Fixed Clear Linux Proxies

--- virtualbox/ClearLinux/setup/bootstrap.sh
+++ virtualbox/ClearLinux/setup/bootstrap.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-# 
-# Install Docker/CContatiners
+# Created by: Karl Vietmeier
+# Install Docker/Clear Containers
 # https://github.com/clearcontainers/runtime/blob/master/docs/clearlinux-installation-guide.md
 
 echo ""
 echo "Running bootstrap.sh"
 echo ""
 
+tee unsetproxy.sh  << EOF > /dev/null 2>&1 
+http_proxy=""
+https_proxy=""
+no_proxy=""
+HTTP_PROXY=""
+HTTPS_PROXY=""
+NO_PROXY=""
+export http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY
+EOF
+
+sudo mkdir -p /etc/swupd
+echo 'https://linux-ftp.jf.intel.com/pub/mirrors/clearlinux/update/' | sudo tee /etc/swupd/mirror_contenturl
+echo 'https://linux-ftp.jf.intel.com/pub/mirrors/clearlinux/update/' | sudo tee /etc/swupd/mirror_versionurl
+
+dos2unix installcerts.sh > /dev/null 2>&1
+sudo bash installcerts.sh > /dev/null 2>&1
+
 # Install container bundle
 swupd bundle-add containers-virt
 
-# Start and enable Dockre and CC3 services
+# Start and enable Docker and Clear Container services
 systemctl enable docker
 systemctl start docker
 systemctl enable cc3-proxy
