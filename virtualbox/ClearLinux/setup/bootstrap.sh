807922889cc6d558576088072100c53a68a99adb
Karl Vietmeier <9323750+kvietmeier@users.noreply.github.com>
2018-07-13 21:10:39 -0700

    Updating Clear Linux

--- virtualbox/ClearLinux/setup/bootstrap.sh
+++ virtualbox/ClearLinux/setup/bootstrap.sh
@@ -2,11 +2,11 @@
 # Created by: Karl Vietmeier
 # Install Docker/Clear Containers
 # https://github.com/clearcontainers/runtime/blob/master/docs/clearlinux-installation-guide.md
+# This needs lots of work a basic skeleton only
 
 echo ""
-echo "Running bootstrap.sh"
+echo "Running Clear Linux bootstrap.sh"
 echo ""
-
 
 # Proxies were giving me grief
 tee unsetproxy.sh  << EOF > /dev/null 2>&1 
@@ -19,11 +19,13 @@ NO_PROXY=""
 export http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY
 EOF
 
+# A fix for my local mirrors - you won't find these in the git repo
 dos2unix installcerts.sh > /dev/null 2>&1
 sudo bash installcerts.sh > /dev/null 2>&1
 
-# Install container bundle
-swupd bundle-add containers-virt
+# Install container bundle and things the Vagrant box might be missing
+swupd bundle-add containers-virt > /dev/null 2>&1
+swupd bundle-add git > /dev/null 2>&1
 
 # Start and enable Docker and Clear Container services
 systemctl enable docker
